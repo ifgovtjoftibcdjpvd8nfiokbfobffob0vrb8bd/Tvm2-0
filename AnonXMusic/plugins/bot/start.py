673c2c05893baf64b407417ebdbcf87d0622565e
Block2002 <marwin2005op@gmail.com>
2025-02-09 10:42:35 +0530

    Update start.py

--- AnonXMusic/plugins/bot/start.py
+++ AnonXMusic/plugins/bot/start.py
@@ -1,5 +1,5 @@
 import time
-import random 
+
 from pyrogram import filters
 from pyrogram.enums import ChatType
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
@@ -23,54 +23,17 @@ from AnonXMusic.utils.inline import help_pannel, private_panel, start_panel
 from config import BANNED_USERS
 from strings import get_string
 
-IMAGE = [
-"https://te.legra.ph/file/7757731c3e8b784b6a550.png", 
-"https://te.legra.ph/file/58c34981e21180989887c.png", 
-"https://te.legra.ph/file/a3a874be5095d9af685ac.png", 
-"https://te.legra.ph/file/ac461a1889255424420ff.png", 
-"https://te.legra.ph/file/74a8ba5270d0e27ac045c.png", 
-"https://te.legra.ph/file/c0d0ee1452cbbbce116f4.png", 
-"https://te.legra.ph/file/d373ae93502a5ae7fd403.png", 
-"https://te.legra.ph/file/ab243bcad20965f637b5c.png", 
-"https://te.legra.ph/file/fd9cc86239dd76d564d01.png", 
-"https://te.legra.ph/file/c12a0b77178e2d2e27a50.png", 
-"https://te.legra.ph/file/35177bbb5d5f07ad8e394.png", 
-"https://te.legra.ph/file/700af8c3ee786a20aff35.png", 
-"https://te.legra.ph/file/cbecd8af0446a422a95ca.png", 
-"https://te.legra.ph/file/c3a0fde4abde25dd25e26.png", 
-"https://te.legra.ph/file/7be8c2f9e093f695c4c6e.png", 
-"https://te.legra.ph/file/ee10888e828bae3a6a0fc.png", 
-"https://te.legra.ph/file/1b55fe681163188149fa4.png", 
-"https://te.legra.ph/file/30ee4e96f64cd9abb69b6.png", 
-"https://te.legra.ph/file/30b121ce5fa87360692ba.png", 
-"https://te.legra.ph/file/f0617cc52008bd78f1a9d.png", 
-"https://te.legra.ph/file/1cd1adc3eb9ac0a101610.png",
-"https://te.legra.ph/file/860c3dd149f91eb450d5a.png", 
-"https://te.legra.ph/file/2e9df77f8100e0327ba52.png",
-"https://te.legra.ph/file/639efe98c133d71c418db.png",
-"https://te.legra.ph/file/8a834586b677739b86bff.png",
-"https://te.legra.ph/file/13f79674ce777f43871fb.png", 
-"https://te.legra.ph/file/147157eca055a1e2c8756.png",
-"https://te.legra.ph/file/b774a8da74dc954afebc6.png",
-"https://te.legra.ph/file/7ae4a6a6a6c28f9f08ceb.png",
-"https://te.legra.ph/file/12d5ea64ed00416a38ec8.png"   
-
-]
-
-
 
 @app.on_message(filters.command(["start"]) & filters.private & ~BANNED_USERS)
 @LanguageStart
 async def start_pm(client, message: Message, _):
     await add_served_user(message.from_user.id)
-    await message.react("🔥")
     if len(message.text.split()) > 1:
         name = message.text.split(None, 1)[1]
         if name[0:4] == "help":
             keyboard = help_pannel(_)
-            await message.reply_sticker("CAACAgUAAxkBAAEBAjFnjyXvMA3Msvtmzi8xB7-HyWPfiQACeQ8AAmXVuVcFtbVH3808UzYE")
             return await message.reply_photo(
-                random.choice(IMAGE),
+                photo=config.START_IMG_URL,
                 caption=_["help_1"].format(config.SUPPORT_CHAT),
                 reply_markup=keyboard,
             )
@@ -79,7 +42,7 @@ async def start_pm(client, message: Message, _):
             if await is_on_off(2):
                 return await app.send_message(
                     chat_id=config.LOGGER_ID,
-                    text=f"❀ {message.from_user.mention} ᴊᴜsᴛ sᴛᴀʀᴛᴇᴅ ᴛʜᴇ ʙᴏᴛ ᴛᴏ ᴄʜᴇᴄᴋ <b>sᴜᴅᴏʟɪsᴛ</b>.\n\n<b>● ᴜsᴇʀ ɪᴅ ➥</b> <code>{message.from_user.id}</code>\n<b>● ᴜsᴇʀɴᴀᴍᴇ ➥</b> @{message.from_user.username}",
+                    text=f"{message.from_user.mention} ᴊᴜsᴛ sᴛᴀʀᴛᴇᴅ ᴛʜᴇ ʙᴏᴛ ᴛᴏ ᴄʜᴇᴄᴋ <b>sᴜᴅᴏʟɪsᴛ</b>.\n\n<b>ᴜsᴇʀ ɪᴅ :</b> <code>{message.from_user.id}</code>\n<b>ᴜsᴇʀɴᴀᴍᴇ :</b> @{message.from_user.username}",
                 )
             return
         if name[0:3] == "inf":
@@ -117,20 +80,19 @@ async def start_pm(client, message: Message, _):
             if await is_on_off(2):
                 return await app.send_message(
                     chat_id=config.LOGGER_ID,
-                    text=f"❀ {message.from_user.mention} ᴊᴜsᴛ sᴛᴀʀᴛᴇᴅ ᴛʜᴇ ʙᴏᴛ ᴛᴏ ᴄʜᴇᴄᴋ <b>ᴛʀᴀᴄᴋ ɪɴғᴏʀᴍᴀᴛɪᴏɴ</b>.\n\n<b>● ᴜsᴇʀ ɪᴅ ➥</b> <code>{message.from_user.id}</code>\n<b>● ᴜsᴇʀɴᴀᴍᴇ ➥</b> @{message.from_user.username}",
+                    text=f"{message.from_user.mention} ᴊᴜsᴛ sᴛᴀʀᴛᴇᴅ ᴛʜᴇ ʙᴏᴛ ᴛᴏ ᴄʜᴇᴄᴋ <b>ᴛʀᴀᴄᴋ ɪɴғᴏʀᴍᴀᴛɪᴏɴ</b>.\n\n<b>ᴜsᴇʀ ɪᴅ :</b> <code>{message.from_user.id}</code>\n<b>ᴜsᴇʀɴᴀᴍᴇ :</b> @{message.from_user.username}",
                 )
     else:
         out = private_panel(_)
-        await message.reply_sticker("CAACAgUAAxkBAAEBAjFnjyXvMA3Msvtmzi8xB7-HyWPfiQACeQ8AAmXVuVcFtbVH3808UzYE")
         await message.reply_photo(
-            random.choice(IMAGE),
+            photo=config.START_IMG_URL,
             caption=_["start_2"].format(message.from_user.mention, app.mention),
             reply_markup=InlineKeyboardMarkup(out),
         )
         if await is_on_off(2):
             return await app.send_message(
                 chat_id=config.LOGGER_ID,
-                text=f"❀ {message.from_user.mention} ᴊᴜsᴛ sᴛᴀʀᴛᴇᴅ ᴛʜᴇ ʙᴏᴛ.\n\n<b>● ᴜsᴇʀ ɪᴅ ➥</b> <code>{message.from_user.id}</code>\n<b>● ᴜsᴇʀɴᴀᴍᴇ ➥</b> @{message.from_user.username}",
+                text=f"{message.from_user.mention} ᴊᴜsᴛ sᴛᴀʀᴛᴇᴅ ᴛʜᴇ ʙᴏᴛ.\n\n<b>ᴜsᴇʀ ɪᴅ :</b> <code>{message.from_user.id}</code>\n<b>ᴜsᴇʀɴᴀᴍᴇ :</b> @{message.from_user.username}",
             )
 
 
@@ -140,7 +102,7 @@ async def start_gp(client, message: Message, _):
     out = start_panel(_)
     uptime = int(time.time() - _boot_)
     await message.reply_photo(
-        random.choice(IMAGE),
+        photo=config.START_IMG_URL,
         caption=_["start_1"].format(app.mention, get_readable_time(uptime)),
         reply_markup=InlineKeyboardMarkup(out),
     )
@@ -175,7 +137,7 @@ async def welcome(client, message: Message):
 
                 out = start_panel(_)
                 await message.reply_photo(
-                    random.choice(IMAGE),
+                    photo=config.START_IMG_URL,
                     caption=_["start_3"].format(
                         message.from_user.first_name,
                         app.mention,
@@ -188,4 +150,3 @@ async def welcome(client, message: Message):
                 await message.stop_propagation()
         except Exception as ex:
             print(ex)
-            
